3fafea12e2fd5aa155d5ebf0467cd895988a5ac7
salim toudite <salim.toudite@gmail.com>
2020-06-13 19:14:37 +0100

    Backend First Commit

---
+++ Cinema/src/main/java/org/sid/CinemaApplication.java
@@ -0,0 +1,40 @@
+package org.sid;
+
+import org.sid.entities.Film;
+import org.sid.entities.Salle;
+import org.sid.entities.Ticket;
+import org.sid.service.ICinemaInitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CinemaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private ICinemaInitService cinemaInitService;
+	
+	@Autowired
+	private RepositoryRestConfiguration restConfiguration;
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		restConfiguration.exposeIdsFor(Film.class,Salle.class,Ticket.class);
+		cinemaInitService.initVilles();
+		cinemaInitService.initCinemas();
+		cinemaInitService.initSalles();
+		cinemaInitService.initPlaces();
+		cinemaInitService.initSeances();
+		cinemaInitService.initCategories();
+		cinemaInitService.initFilms();
+		cinemaInitService.initProjections();
+		cinemaInitService.initTickets();
+	}
+	
+
+}
